97768ff208783e974405a39a61c996ea10f16db4
ZhengChengkai826 <zchengkai826@gmail.com>
2019-04-03 12:14:16 +0800

    now can add multiple column definition div

--- assets/util.js
+++ assets/util.js
@@ -1,7 +1,7 @@
 function createSelector(arr) {
-    selector = document.createElement("SELECT");
-    for (var val of arr) {
-        option = document.createElement("OPTION");
+    let selector = document.createElement("SELECT");
+    for (let val of arr) {
+        let option = document.createElement("OPTION");
         option.setAttribute("value", val);
         option.innerText = val;
         selector.appendChild(option);
@@ -9,37 +9,55 @@ function createSelector(arr) {
     return selector;
 }
 
+function createColumnDefinitionDiv() {
+    let columnDefinitionDiv = document.createElement("DIV");
+
+    let types = ['(type)', 'bigint', 'bigserial', 'bit', 'bit varying', 'boolean',
+        'box', 'bytea', 'character', 'character varying', 'cidr', 'circle', 'date',
+        'double precision', 'inet', 'integer', 'interval', 'json', 'jsonb', 'line',
+        'lseg', 'macaddr', 'money', 'numeric', 'path', 'pg_lsn', 'point', 'polygon',
+        'real', 'smallint', 'smallserial', 'serial', 'text', 'time',
+        'timestamp', 'tsquery', 'tsvector', 'txid_snapshot', 'uuid', 'xml'];
+    let typeSelector = createSelector(types);
+    columnDefinitionDiv.appendChild(typeSelector);
+
+    let nameInput = document.createElement("INPUT");
+    nameInput.setAttribute("value", "(name)");
+    columnDefinitionDiv.appendChild(nameInput);
+
+    columnDefinitionDiv.appendChild(createSelector(['(options)', 'NOT NULL', 'DEFAULT']));
+
+    let additionalInput = document.createElement("INPUT");
+    additionalInput.setAttribute("value", "(additional value)");
+    columnDefinitionDiv.appendChild(additionalInput);
+
+    let addButton = document.createElement("BUTTON");
+    addButton.setAttribute("type", "button");
+    addButton.setAttribute("onclick", "addColumnDefinitionDiv()");
+    addButton.innerText = "Add";
+    columnDefinitionDiv.appendChild(addButton);
+
+    return columnDefinitionDiv;
+}
+
+function addColumnDefinitionDiv() {
+    let varOptionsDiv = document.getElementById("var options");
+    varOptionsDiv.appendChild(createColumnDefinitionDiv());
+}
+
 function prepareInputs() {
-    var varOptionsDiv = document.getElementById("var options");
+    let varOptionsDiv = document.getElementById("var options");
     while(varOptionsDiv.firstChild) {
         varOptionsDiv.removeChild(varOptionsDiv.firstChild);
     }
 
-    var cmdCategorySelector = document.getElementById("cmd category selector");
-    var cmdCategoryId = cmdCategorySelector.selectedIndex;
-    var cmdCategoryName = cmdCategorySelector.options[cmdCategoryId].getAttribute("value");
+    let cmdCategorySelector = document.getElementById("cmd category selector");
+    let cmdCategoryId = cmdCategorySelector.selectedIndex;
+    let cmdCategoryName = cmdCategorySelector.options[cmdCategoryId].getAttribute("value");
 
     switch(cmdCategoryName) {
         case "CREATE TABLE":
-            var types = ['(type)', 'bigint', 'bigserial', 'bit', 'bit varying', 'boolean',
-            'box', 'bytea', 'character', 'character varying', 'cidr', 'circle', 'date',
-            'double precision', 'inet', 'integer', 'interval', 'json', 'jsonb', 'line',
-            'lseg', 'macaddr', 'money', 'numeric', 'path', 'pg_lsn', 'point', 'polygon',
-            'real', 'smallint', 'smallserial', 'serial', 'text', 'time',
-            'timestamp', 'tsquery', 'tsvector', 'txid_snapshot', 'uuid', 'xml'];
-            var typeSelector = createSelector(types);
-            varOptionsDiv.appendChild(typeSelector);
-
-            var nameInput = document.createElement("INPUT");
-            nameInput.setAttribute("value", "(name)");
-            varOptionsDiv.appendChild(nameInput);
-
-            varOptionsDiv.appendChild(createSelector(['(options)', 'NOT NULL', 'DEFAULT']));
-            varOptionsDiv.appendChild(document.createElement("BR"));
-            var addButton = document.createElement("BUTTON");
-            addButton.setAttribute("type", "button");
-            addButton.innerText = "Add";
-            varOptionsDiv.appendChild(addButton);
+            addColumnDefinitionDiv();
             break;
         default:
             break;
